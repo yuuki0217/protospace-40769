350013a9f5bc9c88a72cbfc0a34bc29666b94391
yuuki0217 <Growthenvironment19oka@gmail.com>
2024-07-23 21:54:26 +0900

    コメント機能の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,8 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else 
-      @prototype = Prototype.find(params[:id])
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
       render "prototypes/show", status: :unprocessable_entity
     end
   end
